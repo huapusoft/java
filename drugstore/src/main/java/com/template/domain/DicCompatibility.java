a95bb86576fc652caab0d730a41fb0139218fce5
578764763@qq.com <578764763@qq.com>
2016-04-18 13:24:15 +0800

    fengql

--- drugstore/src/main/java/com/template/domain/DicCompatibility.java
+++ drugstore/src/main/java/com/template/domain/DicCompatibility.java
@@ -1,62 +1,51 @@
 package com.template.domain;
 
+import java.util.List;
+
 /**
- * 配伍表实体
+ * 配伍主表实体
 * @author  fengql 
 * @date 2016年4月14日 上午9:39:26
  */
 public class DicCompatibility {
 
 	private int id;//唯一，自增长
-	private int comTypeA;//A类：0功能代码,1药类id
-	private String functionCodeA;//A类功能代码
-	private int drugClassIdA;//A类药类id
-	private int comTypeB;//B类：0功能代码,1药类id
-	private String functionCodeB;//B类功能代码
-	private int drugClassIdB;//B类药类id
+	private int comId;//配伍关联id
+	private String contentA;//A类内容
+	private String contentB;//B类内容
 	private String result;//配伍结果
 	
+	private List<DicCompatibilityDetail> detailList;//配伍明细
+	
+	public List<DicCompatibilityDetail> getDetailList() {
+		return detailList;
+	}
+	public void setDetailList(List<DicCompatibilityDetail> detailList) {
+		this.detailList = detailList;
+	}
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getComTypeA() {
-		return comTypeA;
+	public int getComId() {
+		return comId;
 	}
-	public void setComTypeA(int comTypeA) {
-		this.comTypeA = comTypeA;
+	public void setComId(int comId) {
+		this.comId = comId;
 	}
-	public String getFunctionCodeA() {
-		return functionCodeA;
+	public String getContentA() {
+		return contentA;
 	}
-	public void setFunctionCodeA(String functionCodeA) {
-		this.functionCodeA = functionCodeA;
+	public void setContentA(String contentA) {
+		this.contentA = contentA;
 	}
-	public int getDrugClassIdA() {
-		return drugClassIdA;
+	public String getContentB() {
+		return contentB;
 	}
-	public void setDrugClassIdA(int drugClassIdA) {
-		this.drugClassIdA = drugClassIdA;
-	}
-	public int getComTypeB() {
-		return comTypeB;
-	}
-	public void setComTypeB(int comTypeB) {
-		this.comTypeB = comTypeB;
-	}
-	public String getFunctionCodeB() {
-		return functionCodeB;
-	}
-	public void setFunctionCodeB(String functionCodeB) {
-		this.functionCodeB = functionCodeB;
-	}
-	public int getDrugClassIdB() {
-		return drugClassIdB;
-	}
-	public void setDrugClassIdB(int drugClassIdB) {
-		this.drugClassIdB = drugClassIdB;
+	public void setContentB(String contentB) {
+		this.contentB = contentB;
 	}
 	public String getResult() {
 		return result;
@@ -64,5 +53,6 @@ public class DicCompatibility {
 	public void setResult(String result) {
 		this.result = result;
 	}
+	
 	
 }
